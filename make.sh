5b256e0395a334f3a45177b3ccd6f8bce550ab7f
John McDonnell <john.v.mcdonnell@gmail.com>
2011-04-15 01:53:44 -0400

    Deleted a bunch of junk from ipython, make.sh should work at this point.

--- make.sh
+++ make.sh
@@ -2,9 +2,23 @@
 
 for file in `git ls-files`;
 do 
-	if [[ `expr substring $file 1 6` -eq "source" ]]; then
-		echo $file
-		# ginstall -p -D -m 440 $file ~/.$file
-		#ginstall -p -D -m 440 $file
+	case "$file" in
+		README)
+			continue
+			;;
+	esac
+	
+	if [[ `expr $file : "source"` -ne 0 ]];
+	then
+		echo ~/$file
+		#ginstall -p -D -m 440 $file ~/$file
+	elif [[ `expr $file : "config"` -ne 0 ]]; 
+	then
+		echo ~/$file
+		#ginstall -p -D -m 440 $file ~/$file
+	else
+		echo ~/.$file
+		#ginstall -p -D -m 440 $file ~/.$file
 	fi
 done
+
